61bf8d2c00df88c9071eb82475de9057b6c632da
Toan Nguyen <hgiasac@gmail.com>
2024-04-16 01:06:28 +0700

    ci: add release pipeline  (#1)

---
+++ scripts/build-manifest.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -evo pipefail
+
+REF="$(git rev-parse --short HEAD)"
+VERSION=${VERSION:-$REF}
+BUILD_DIR=/tmp/ndc-stripe
+ROOT="$(pwd)"
+
+rm -rf $BUILD_DIR
+mkdir -p $BUILD_DIR
+
+cp -r connector-definition $BUILD_DIR
+sed -i "s/{{VERSION}}/$VERSION/g" $BUILD_DIR/connector-definition/.hasura-connector/connector-metadata.yaml
+
+mkdir -p "${ROOT}/release"
+tar -czvf "${ROOT}/release/connector-definition.tgz" --directory $BUILD_DIR/connector-definition .
+echo "checksum of connector-definition.tgz:"
+sha256sum "${ROOT}/release/connector-definition.tgz"
---
+++ scripts/test.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+ROOT="$(pwd)"
+NDC_TEST_VERSION=v0.1.2
+
+http_wait() {
+  printf "$1:\t "
+  for i in {1..60};
+  do
+    local code="$(curl -s -o /dev/null -m 2 -w '%{http_code}' $1)"
+    if [[ $code != "200" ]]; then
+      printf "."
+      sleep 1
+    else
+      printf "\r\033[K$1:\t ${GREEN}OK${NC}\n"
+      return 0
+    fi
+  done
+  printf "\n${RED}ERROR${NC}: cannot connect to $1. Please check service logs with command:\n"
+  echo "  docker-compose logs -f $2"
+  exit 1
+}
+
+# download ndc-test
+if [ ! -f "$ROOT/ndc-test" ]; then 
+  curl -L "https://github.com/hasura/ndc-spec/releases/download/$NDC_TEST_VERSION/ndc-test-x86_64-unknown-linux-gnu" -o "$ROOT/ndc-test"
+  chmod +x "$ROOT/ndc-test"
+fi
+
+cp .env.example .env
+DOCKER_BUILDKIT=1 COMPOSE_DOCKER_CLI_BUILD=1 docker compose up -d
+http_wait http://localhost:8080/health
+
+$ROOT/ndc-test test --endpoint http://localhost:8080 --snapshots-dir $ROOT/testdata/mock
+$ROOT/ndc-test replay --endpoint http://localhost:8080 --snapshots-dir $ROOT/testdata/mock
+
+exit_code=$?
+docker compose down --remove-orphans
+exit $exit_code
